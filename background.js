c32c70f6d8427d0ec8839f79a92b8f7e404de051
roszk008 <roszk008@umn.edu>
2020-05-17 02:06:57 -0500

    doc(background.js): some comments

--- background.js
+++ background.js
@@ -26,17 +26,23 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
       // console.log("oh boy i did a thing");
       break;
     case "lookup-selection":
-      console.log(`Looking up ${info.selectionText}...`);
-      merriamLookup(info.selectionText);
+      // Preprocess selected text
+      const word = info.selectionText.trim().split(' ')[0];
+
+      // Perform the lookup
+      console.log(`Looking up ${word}...`);
+      merriamLookup(word);
       console.log("done!");
       break;
   }
 });
 
+// Makes an HTTP request to Merriam-Webster's API
 function merriamLookup(word) {
   fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=7c41540c-3178-41c3-838c-216c402fd175`).then(response => response.json()).then(workResponse).catch(err => console.error(err));
 }
 
+// Takes in a JSON object which is the dictionary entry for the selected word
 function workResponse(response) {
 
 }
