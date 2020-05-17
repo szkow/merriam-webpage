704d3b25e8f6452c32f538caea4f7a1394c89bf7
roszk008 <roszk008@umn.edu>
2020-05-17 15:38:57 -0500

    fix(all): makes everything "work" in a rudimentary way

--- background.js
+++ background.js
@@ -38,61 +38,29 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
 
       // Perform the lookup
       console.log(`Looking up ${word}...`);
-      merriamLookup(word);
+      merriamLookup(word, tab);
       console.log("done!");
       break;
   }
 });
 
 // Makes an HTTP request to Merriam-Webster's API
-function merriamLookup(word) {
-  fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=7c41540c-3178-41c3-838c-216c402fd175`).then(response => response.json()).then(makeNode).catch(onError);
+function merriamLookup(word, tab) {
+  fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=7c41540c-3178-41c3-838c-216c402fd175`).then(response => response.json()).then(response => sendEntry(response, tab)).catch(onError);
 }
 
 // Takes in a JSON object which is the dictionary entry for the selected word
-function makeNode(response) {
+function sendEntry(response, tab) {
   // Use only the first match for simplicity
   const dictEntry = response[0];
 
   console.log(dictEntry);
-  const word = dictEntry["meta"]["id"];
-  const short = dictEntry["shortdef"];
 
-  // Create the node
-  // var node = document.createElement("div");
-  // var content = document.createTextNode(word + "\n\n" + short[0]);
-  // node.appendChild(content);
-
-  // // node.style.position = "absolute";
-  // node.style.border = "solid red 20px";
-  
-  // document.body.append(node);
-
-  browser.tabs.query({
-    currentWindow: true,
-    active: true
-  }).then(tabs => {
-    for (let tab of tabs) {
-      browser.tabs.sendMessage(
-        tab.id,
-        {content: dictEntry}
-      ).then(response => {
-        console.log("Message from the content script:");
-        console.log(response.response);
-      }).catch(onError);
-    }
+  browser.tabs.sendMessage(
+    tab.id,
+    {content: dictEntry}
+  ).then(response => {
+    console.log("Message from the content script:");
+    console.log(response.response);
   }).catch(onError);
-}
-
-
-function sendMessageToTabs(tabs) {
-  for (let tab of tabs) {
-    browser.tabs.sendMessage(
-      tab.id,
-      {greeting: "Hi from background script"}
-    ).then(response => {
-      console.log("Message from the content script:");
-      console.log(response.response);
-    }).catch(onError);
-  }
 }
--- main.js
+++ main.js
@@ -8,5 +8,59 @@ browser.runtime.onMessage.addListener(request => {
 });
 
 function createPopup(dictEntry) {
+  // Extract the content we need
+  const word = dictEntry["meta"]["id"];
+  const short = dictEntry["shortdef"];
+
+  // Find coordinates of word on page
+  // const position = getSelectionCoords(window);
+  const selection = window.getSelection();
+  const anchorOffset = selection.anchorOffset;
+  const anchor = selection.anchorNode;
+  const parentAnchor = anchor.parentNode;
+  const boundingRect = selection.getRangeAt(0).getBoundingClientRect();
+
+  // Create the node
+  var node = document.createElement("div");
+  var content = document.createTextNode(word + "\n\n" + short[0]);
+  node.appendChild(content);
+
+  node.style.position = "absolute";
+  node.style.left = `${boundingRect.x + window.scrollX - 0.5 * node.clientWidth}px`;
+  node.style.top = `${boundingRect.y + window.scrollY}px`;
+  node.style.border = "solid red 20px";
   
+  // parentAnchor.insertBefore(node, anchor);
+  document.body.append(node);
+}
+
+// From https://stackoverflow.com/questions/6846230/coordinates-of-selected-text-in-browser-page 
+function getSelectionCoords(win) {
+  win = win || window;
+  var doc = win.document;
+  var sel = doc.selection, range, rects, rect;
+  var x = 0, y = 0;
+  if (sel) {
+    if (sel.type != "Control") {
+      range = sel.createRange();
+      range.collapse(true);
+      x = range.boundingLeft;
+      y = range.boundingTop;
+    }
+  } else if (win.getSelection) {
+    sel = win.getSelection();
+    if (sel.rangeCount) {
+      range = sel.getRangeAt(0).cloneRange();
+      if (range.getClientRects) {
+        range.collapse(true);
+        rects = range.getClientRects();
+        if (rects.length > 0) {
+          rect = rects[0];
+        }
+        x = rect.left;
+        y = rect.top;
+      }
+    }
+  }
+  return {x: x, y: y};
 }
